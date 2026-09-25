3533d9c35dd608fda0d9f173cbf0867623754131
EfrenMedina <101607349+EfrenMedina@users.noreply.github.com>
2026-09-25 10:04:25 -0400

    Completed Exercise 2: FirstLetters

--- exercises/ex02-first-letters/src/main/java/FirstLetters.java
+++ exercises/ex02-first-letters/src/main/java/FirstLetters.java
@@ -25,7 +25,11 @@ public class FirstLetters {
      * @return the first character of each word, concatenated
      */
     public static String firstLetters(String words) {
-        // TODO: complete
-        return "";
+        String[] splitted = words.split(" ");
+        StringBuilder answer = new StringBuilder();
+        for (String word: splitted){
+            answer.append(word.charAt(0));
+        }
+        return answer.toString();
     }
 }
